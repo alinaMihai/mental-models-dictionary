46269c9769822cba742b484401f1b3f200523972
alinaMihai <alina_mihai90@yahoo.com>
2020-07-04 22:36:20 +0300

    attempt to fix server

--- server/config/express.js
+++ server/config/express.js
@@ -8,12 +8,6 @@ const config = require('./environment');
 module.exports = function(app) {
     app.set('appPath', path.join(config.root, 'build'));
     app.use(favicon(path.join(config.root, 'public/favicon.ico')));
-    app.use(express.static(path.join(__dirname, 'build')));
-
-
-    app.get('/*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'));
-    });
 
     if (config.host === 'localhost') {
         app.use(express.static(path.join(config.root, 'build')));
