0eb677f52608024f30a7365072fbfaafbf660f90
alinaMihai <alina_mihai90@yahoo.com>
2020-07-04 21:50:56 +0300

    fix deploy issue

--- server/config/express.js
+++ server/config/express.js
@@ -5,10 +5,16 @@ const favicon = require('serve-favicon');
 const config = require('./environment');
 
 module.exports = function(app) {
-    app.set('appPath', path.join(config.root, 'dist'));
+    app.set('appPath', path.join(config.root, 'build'));
     app.use(favicon(path.join(config.root, 'public/favicon.ico')));
+    app.use(express.static(path.join(__dirname, 'build')));
+
+
+    app.get('/*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    });
 
     if (config.host === 'localhost') {
-        app.use(express.static(path.join(config.root, 'dist')));
+        app.use(express.static(path.join(config.root, 'build')));
     }
 }
