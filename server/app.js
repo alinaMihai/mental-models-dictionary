c4aa705a1a2a3565d72c0ab787c401366dd3f4cd
alinaMihai <alina_mihai90@yahoo.com>
2020-07-04 22:56:31 +0300

    fix server serving files

--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const config = require('./config/environment');
+var bodyParser = require('body-parser');
 const app = express();
-const server = require('http').createServer(app);
-require('./config/express')(app);
 require('./routes')(app);
+require('./config/express')(app);
+const server = require('http').createServer(app);
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json({ limit: '50mb' }));
 
 server.listen(config.port, config.ip, function() {
     console.log(
--- server/config/express.js
+++ server/config/express.js
@@ -8,6 +8,12 @@ const config = require('./environment');
 module.exports = function(app) {
     app.set('appPath', path.join(config.root, 'build'));
     app.use(favicon(path.join(config.root, 'public/favicon.ico')));
+    app.use(express.static(path.join(config.root, 'build')));
+
+    // All other routes should redirect to the index.html
+    app.route('/*').get(function(req, res) {
+        res.sendFile(path.resolve(app.get('appPath') + '/index.html'));
+    });
 
     if (config.host === 'localhost') {
         app.use(express.static(path.join(config.root, 'build')));
